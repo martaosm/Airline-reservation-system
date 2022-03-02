5037ac27043fe79c1bf37403de3690e3ab4a862a
Marta <marta.osm00@gmail.com>
2022-03-02 16:47:15 +0100

    Added diplaying booked flights and total price

--- exercises/src/main/java/com/mycompany/exercises/main.java
+++ exercises/src/main/java/com/mycompany/exercises/main.java
@@ -6,11 +6,7 @@ package com.mycompany.exercises;
 
 import com.mycompany.exercises.model.model;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
-
-
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -23,27 +19,52 @@ public class main {
      */
     public static void main(String[] args) {
         
-        Scanner myObj = new Scanner(System.in);  
-        System.out.println("Start location: ");
-        String start = myObj.nextLine();
-        System.out.println("End location: ");
-        String end = myObj.nextLine();
-        
         ArrayList<model> tab = new ArrayList<>();
         
+        ArrayList<model> bookedFlights = new ArrayList<>();
+        
+        int totalPrice = 0;
+        
+        String start = JOptionPane.showInputDialog("Start destination: ");
+        String end = JOptionPane.showInputDialog("End destination: ");
+
         for(int i=0;i<10;i++){
             model flight = new model(end);
             tab.add(flight);
         }
         
+        String[] displayArray = new String[tab.size()];
         for(int i=0;i<10;i++){
             System.out.println("Date: " + tab.get(i).date + ", Hour: " + tab.get(i).hour + ", Flight number: " + tab.get(i).flight_number + ", Price: " + tab.get(i).price + ", Duration: " + tab.get(i).duration + "h");
+            String display = "Date: " + tab.get(i).date + ", Hour: " + tab.get(i).hour + ", Flight number: " + tab.get(i).flight_number + ", Price: " + tab.get(i).price + ", Duration: " + tab.get(i).duration + "h";
+            displayArray[i]=display;
         }
-        /*long aDay = TimeUnit.DAYS.toMillis(1);
-        long now = new Date().getTime();
-        Date hundredYearsAgo = new Date(now + aDay * 365);
-        Date random = RandomDates.between(now, hundredYearsAgo);
-        assertThat(random).isBetween(now, hundredYearsAgo);*/
+        
+        model[] tab2 = new model[tab.size()];
+        for(int i=0;i<tab.size();i++){
+            tab2[i]=tab.get(i);
+        }
+        Object[] values = displayArray;
+        Object selectedValue = JOptionPane.showInputDialog(null,"Choose one", "Input",JOptionPane.INFORMATION_MESSAGE, null, values, values[0]);
+        
+        for(int i=0;i<tab.size();i++){
+            if(selectedValue == displayArray[i]){
+                bookedFlights.add(tab.get(i));
+                break;
+            }
+        }
+        
+        System.out.println("-------------------------------------------------\nBooked flights: ");
+        for(int i=0;i<bookedFlights.size();i++){
+            System.out.println("Date: " + bookedFlights.get(i).date + ", Hour: " + bookedFlights.get(i).hour + ", Flight number: " + bookedFlights.get(i).flight_number + ", Price: " + bookedFlights.get(i).price + ", Duration: " + bookedFlights.get(i).duration + "h");
+        }
+
+        for(int i=0;i<bookedFlights.size();i++){
+            totalPrice = totalPrice + bookedFlights.get(i).price;
+        }
+        
+        System.out.println("Your total: " + totalPrice);
+        
     }
     
 }
